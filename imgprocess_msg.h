0897f21d2e468fddfeb598e7c5336a9cc3035353
Ali Kanarya <akanarya@gmail.com>
2010-12-20 00:23:31 +0200

    1.0.000
    initial commit

---
+++ imgprocess_msg.h
@@ -0,0 +1,12 @@
+#ifndef IMGPROCESS_MSG_H
+#define IMGPROCESS_MSG_H
+
+#define message0    "Herþey normal"
+
+#define alarm1      "Lazer izi tespit edilemedi !!! (Eþik deðerinden az)"
+#define alarm2      "Hiç aralýk bulunamadý !!!"
+#define alarm3      "Aralýk miktarý eþik deðerinden az !!!"
+#define alarm4      "Bulunan aralýk imajýn kenarlarýna çok yakýn !!!"
+#define alarm5      "Lazer açýsý çok fazla !!!"
+
+#endif // IMGPROCESS_MSG_H
